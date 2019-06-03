72a094a9832ec3a43c3dd282dd53e25159f5b6b2
Asteele7301994 <andrew.steele73094@gmail.com>
2019-06-03 08:20:06 -0400

    css update

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -28,3 +28,8 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
